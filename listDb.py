02a416a72baefdd2593eb86feb841bb271a62982
nmdeni <mekadeni@mail.ru>
2023-01-31 00:35:32 +0300

    Подправил вывод из дб

--- listDb.py
+++ listDb.py
@@ -7,8 +7,8 @@ def list_output(db_cursor):
 
     for k in db_cursor.fetchall():
         if k[3] == True:
-            table_db += f"ID {k[0]}|\t{k[1]}\t{k[2]}₽\tВЫ должны\n"
+            table_db += f"{k[0]}   {k[1]}   {k[2]}₽   ВЫ должны\n"
         else:
-            table_db += f"ID {k[0]}|\t{k[1]}\t{k[2]}₽\tВАМ должны\n"
+            table_db += f"{k[0]}   {k[1]}   {k[2]}₽   ВАМ должны\n"
 
     return table_db
